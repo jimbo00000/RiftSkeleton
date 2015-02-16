97ef4489a435d53ffd2f5fe800b6ea261186d621
jimbo00000 <james.susinno@gmail.com>
2015-02-16 14:54:16 -0500

    Express mono modelview matrix calculation as the inverse of the result of the makeChassisMatrix_glm function.

--- src/AppSkeleton/AppSkeleton.cpp
+++ src/AppSkeleton/AppSkeleton.cpp
@@ -232,8 +232,7 @@ void AppSkeleton::_drawSceneMono() const
 
     const glm::mat4 mvLocal = glm::mat4(1.f);
     const glm::mat4 mvWorld = mvLocal *
-        glm::rotate(glm::mat4(1.f), m_chassisYaw, glm::vec3(0.f, 1.f, 0.f)) *
-        glm::translate(glm::mat4(1.f), -m_chassisPos);
+        glm::inverse(makeChassisMatrix_glm(m_chassisYaw, m_chassisPos));
 
     const glm::ivec2 vp = getRTSize();
     const glm::mat4 persp = glm::perspective(
