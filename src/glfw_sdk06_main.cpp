46812478852c9f754f17fbdd8dd489d94cf45f7a
Jim Susinno <jim@jimbomania.com>
2015-09-02 22:41:30 -0400

    Take out linux headers in glfw_sdk06_main.cpp.

--- src/glfw_sdk06_main.cpp
+++ src/glfw_sdk06_main.cpp
@@ -7,11 +7,6 @@
 #  include <Windows.h>
 #  define GLFW_EXPOSE_NATIVE_WIN32
 #  define GLFW_EXPOSE_NATIVE_WGL
-#elif defined(__linux__)
-#  include <X11/X.h>
-#  include <X11/extensions/Xrandr.h>
-#  define GLFW_EXPOSE_NATIVE_X11
-#  define GLFW_EXPOSE_NATIVE_GLX
 #endif
 
 #include <GLFW/glfw3.h>
