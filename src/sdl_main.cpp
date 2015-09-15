c181e5443b9dd939f729ab797b97dbd9aef80b30
Christoph Haag <haagch+git@frickel.club>
2015-09-15 12:53:24 +0200

    more liberal OpenGL context creation
    It doesn't require OpenGL 4.3. 3.3 is enough for all included shaders.
    
    We don't actually need to specify OpenGL 3.3.
    A major version of 3 is enough because since OpenGL 3.0 contexts can be "forward compatible".

--- src/sdl_main.cpp
+++ src/sdl_main.cpp
@@ -709,9 +709,10 @@ int main(int argc, char** argv)
     if (useOpenGLCoreContext)
     {
         LOG_INFO("Using OpenGL core context.");
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+        SDL_GL_SetAttribute (SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
     }
     else
     {
